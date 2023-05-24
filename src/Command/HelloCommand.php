572d36d86f14c8a5c03df9350ee307a3bc869774
Yosri BAHRI <yosribahri@gmail.com>
2023-05-24 23:06:56 +0200

    fix php versions in ci

--- src/Command/HelloCommand.php
+++ src/Command/HelloCommand.php
@@ -7,12 +7,18 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(
-    name: 'devgine:demo:hello',
-    description: 'Hello command'
-)]
 class HelloCommand extends Command
 {
+    protected static $defaultName = 'devgine:demo:hello';
+
+    protected function configure(): void
+    {
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('Hello command.')
+        ;
+    }
+
     /** @SuppressWarnings(PHPMD.UnusedFormalParameter) */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
