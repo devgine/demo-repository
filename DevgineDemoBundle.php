b617359b5dedd9d0acb8a79cf1364021d6eca910
Yosri BAHRI <yosribahri@gmail.com>
2023-05-24 17:36:54 +0200

    create extension

--- DevgineDemoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Devgine\Demo;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DevgineDemoBundle extends Bundle
-{
-}
---
+++ src/DependencyInjection/DevgineDemoExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Devgine\Demo\DependencyInjection;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class DevgineDemoExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        if (class_exists(Application::class)) {
+            $loader->load('console.xml');
+        }
+    }
+}
